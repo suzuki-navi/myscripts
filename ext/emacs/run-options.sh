85cd47810916c8a294ff6d4d45cdd8453ef327bb
suzuki-navi <suzuki.navi.dev@gmail.com>
2023-08-08 18:25:11 +0900

    fix a bug of emacs

--- ext/emacs/run-options.sh
+++ ext/emacs/run-options.sh
@@ -1,6 +1,9 @@
 if [ -e $HOME/.emacs ]; then
-    docker_run_options="$docker_run_options -v $HOME/.emacs:$HOME/.emacs"
+  docker_run_options+=("-v")
+  docker_run_options+=("$HOME/.emacs:$HOME/.emacs")
+
 fi
 if [ -e $HOME/.emacs.d ]; then
-    docker_run_options="$docker_run_options -v $HOME/.emacs.d:$HOME/.emacs.d"
+  docker_run_options+=("-v")
+  docker_run_options+=("$HOME/.emacs.d:$HOME/.emacs.d")
 fi
